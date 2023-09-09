e0fda5e0ed4f5250edd850b91d62457a1e434a4a
davidmurage <davidmurage415@gmail.com>
2023-09-09 13:15:30 +0300

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Davieadmin</span>
+      <span>© Davie.Dev Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
